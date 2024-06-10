5da2890d026c9682d18596bbcc4da677833197ac
Odpluskwiacz <148152180+Odpluskwiacz@users.noreply.github.com>
2024-06-10 12:06:19 +0000

    Add files via upload

--- levels/level 1/Level 1.gd
+++ levels/level 1/Level 1.gd
@@ -1,5 +1,25 @@
 extends Node2D
 
+var Win = 0
 
 func _on_player_coin_collected():
 	$Label.text = "Coins: " + str($Player.coins)
+
+
+
+
+func _on_door_2_win_condition():
+	Win += 1
+func _on_door_2_no_win_condition():
+	Win -= 1
+func _on_door_no_win_condition():
+	Win -= 1
+func _on_door_win_condition():
+	Win += 1
+
+func testWinCondition():
+	if Win ==2:
+		get_node("CanvasLayer/win menu").show()
+		
+func _process(delta):
+	testWinCondition()
--- objects/Door/door.gd
+++ objects/Door/door.gd
@@ -1,10 +1,31 @@
 extends Area2D
 
+signal WinCondition
+signal NoWinCondition
 
+@export var player_name: String
+
+
+
+
+func _ready():
+	if not player_name.is_empty():
+		if player_name == "Zombie":
+			modulate = Color.GREEN
+		if player_name == "Ghost":
+			modulate = Color.CYAN
 
 
 func _on_body_entered(body):
-	if body.name == "Player":
-		await get_tree().create_timer(0.2).timeout
-		get_tree().paused = true
-		body.onDoorColision()
+	if body.is_in_group("Gracze"):
+		if body.player_name == player_name:
+			$AnimatedSprite2D.play("default")
+			WinCondition.emit()
+
+
+func _on_body_exited(body):
+	if body.is_in_group("Gracze"):
+		if body.player_name == player_name:
+			$AnimatedSprite2D.play_backwards("default")
+			NoWinCondition.emit()
+		
